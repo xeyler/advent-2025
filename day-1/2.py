c6237148ea86c35e800a1f440529061637179e56
Brigham Campbell <me@brighamcampbell.com>
2025-12-01 21:25:11 -0700

    Solve day 1 part 2

---
+++ day-1/2.py
@@ -0,0 +1,14 @@
+dial_pos = 50
+password = 0
+
+with open('input') as f:
+    for line in f:
+        line = line.rstrip()
+        movement = int(line[1:])
+        while movement:
+            movement = movement - 1
+            dial_pos = (dial_pos + (-1 if line[0] == "L" else 1)) % 100
+            if dial_pos == 0:
+                password = password + 1
+
+print(password)
